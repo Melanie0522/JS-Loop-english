a9afb4f93dc4c0ae3881e6c57a6620399c32da05
Melanie0522 <106500812+Melanie0522@users.noreply.github.com>
2022-06-10 08:46:47 +0800

    Update index.js

--- number/index.js
+++ number/index.js
@@ -1,27 +1,8 @@
-// Complete the following procedure, the 'name' variable in all letters to uppercase,
-// output: ' 'HELLO' '.
-var name = 'hello';
-// TODO write your code here...
-var name_upper=name.toUpperCase();
-console.log(name_upper);
-
-
-// Complete the following procedure, capitalizing all the words in the variable sentence 
-//and printing: 'Good Afternoon, Mr Mike.'
-var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
-function firstletterup(str) {    
-    var newStr = str.split(" ");    
-    for(var i = 0; i<newStr.length; i++){
-        newStr[i] = newStr[i].slice(0,1).toUpperCase() + newStr[i].slice(1).toLowerCase();
-    }    
-    return newStr.join(" ");
+for(let i = 1; i<=20; i++){
+    if(i%2==0){
+         console.log(i+" is even number.");
+    }
+    else{
+         console.log(i+" is odd number.");
+    }
 }
-sentence_up=firstletterup(sentence);
-console.log(sentence);
-
-// Complete the following procedure to extract the amount of money variable, output: 20.
-var money = '￥20';
-//TODO write your code here...
-var money_num=money.match(/\d+(.\d+)?/g);
-console.log(money_num);
